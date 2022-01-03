97d4a26a5a9ac98c00e6d28370222112d1de66fe
joinik <1793844527@qq.com>
2022-01-03 17:52:37 +0800

    初始化并梳理项目的结构

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+      <p>  app 根组件 </p>
+      <van-button type="primary">主要按钮</van-button>
+      <van-button type="info">信息按钮</van-button>
+      <van-button type="default">默认按钮</van-button>
+      <van-button type="warning">警告按钮</van-button>
+      <van-button type="danger">危险按钮</van-button>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  
+}
+</script>
+
+<style lang="less" scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
